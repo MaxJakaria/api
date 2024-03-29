0cc801c0aed240e75ef8f8d2c3ec79cd212ccfa8
Md. Jakaria Hosen <maxjakaria825@gmail.in>
2024-03-29 11:11:01 +0600

    Creat MyHomePage stateful wiget

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyhomePage extends StatefulWidget {
+  const MyhomePage({super.key});
+
+  @override
+  State<MyhomePage> createState() => _MyhomePageState();
+}
+
+class _MyhomePageState extends State<MyhomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api Course'),
+      ),
+      body: Column(children: []),
+    );
+  }
+}
